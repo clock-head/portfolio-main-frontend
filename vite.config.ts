4fd83fb0a5836a9a5c8b9f58e065e645d5239c70
clock-head <james.n.kai92@gmail.com>
2025-06-30 18:21:43 +1000

    refactored build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig, type UserConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-
-    css: true,
-  },
   resolve: {
     alias: {
       'athena-core': '/athena-core/dist',
       'athena-core/ref': '/athena-core/dist/ref',
     },
   },
-} satisfies UserConfig);
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+  },
+});
